21ce96a0374da6800657cba283ac938143fda57a
envizar <silent.gate@ya.ru>
2024-12-18 20:45:36 +0300

    ремейк статьи 'Привязка домена к серверу'

---
+++ .vitepress/theme/components/ThemedImg.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { useData } from 'vitepress'
+
+const props = defineProps<{
+  dark: string,
+  light: string,
+  alt?: string
+}>()
+
+const { isDark } = useData()
+</script>
+
+<template>
+  <img :src="isDark ? dark : light" :alt>
+</template>
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -5,11 +5,13 @@ import './style.scss'
 import 'virtual:uno.css'
 import Layout from "./Layout.vue"
 import { enhanceAppWithTabs } from 'vitepress-plugin-tabs/client'
+import ThemedImg from "./components/ThemedImg.vue";
 
 export default {
   extends: DefaultTheme,
   Layout: Layout,
   enhanceApp({ app, router, siteData }) {
     enhanceAppWithTabs(app)
+    app.component('ThemedImg', ThemedImg)
   }
 } satisfies Theme
